b79bcfa86208810fa8126f83dca401c40e98556c
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2019-11-10 01:38:14 -0500

    adding restore of exercises db

--- postgres/create_db.sh
+++ postgres/create_db.sh
@@ -2,3 +2,8 @@ echo "creating the 'dvdrental' database"
 psql -U postgres -c 'create database dvdrental'
 echo "restoring the 'dvdrental' database"
 pg_restore -U postgres -d dvdrental /tmp/dvdrental.tar
+
+echo "creating the 'excercise' database"
+psql -U postgres -c 'create database exercises'
+echo "restoring the 'exercises' database"
+pg_restore -U postgres -d exercises /tmp/exercises.tar
